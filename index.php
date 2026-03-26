c25c762f1ad3c0679cc09b724b10ef385edfc7b7
Alvaro Tomasino <alvaroyaugusto@gmail.com>
2026-03-26 19:04:58 -0300

    Iconos y carousel en archivo independiente

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ function getMainPage()
     rel="stylesheet"
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
     crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.css">
   <link rel="stylesheet" href="src/view/styles/styles.css" />
 </head>
 
