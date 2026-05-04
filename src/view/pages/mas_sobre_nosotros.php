61e2e5d0d6d078e92b6ff419850ab0e8bf59d202
meliaronson <melinaaronson@gmail.com>
2026-05-03 21:06:22 -0300

    arreglo menu hamburguesa que no se veia

--- src/view/pages/mas_sobre_nosotros.php
+++ src/view/pages/mas_sobre_nosotros.php
@@ -9,7 +9,7 @@
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
     rel="stylesheet"
     crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.css">
   <link rel="stylesheet" href="../styles/styles.css" />
 </head>
 
