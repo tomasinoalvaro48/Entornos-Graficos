05f67b5bfd722967f1a3f0293fc499d3714d16aa
meliaronson <melinaaronson@gmail.com>
2026-05-03 21:15:14 -0300

    arreglo navbar en crear novedad

--- src/view/pages/novedad/novedad_create.php
+++ src/view/pages/novedad/novedad_create.php
@@ -96,6 +96,11 @@ $today = new DateTime();
     </section>
   </main>
 
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous">
+  </script>
 </body>
 
 <script>
